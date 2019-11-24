8de607cfccefba13a1dd6a0cba2a327cc7c06063
liming wu <limwuly@gmail.com>
2019-11-23 21:40:42 -0500

    display movie list

--- ps-movies/movie-list.component.js
+++ ps-movies/movie-list.component.js
@@ -3,16 +3,39 @@
 
   var module = angular.module("psMovies");
 
+  function fetchMovies($http) {
+    return $http.get("/movies.json").then(function(res) {
+      return res.data;
+    });
+  }
+
+  function controller($http) {
+    var model = this;
+    model.movies = [];
+
+    model.$onInit = function() {
+      fetchMovies($http).then(function(movies) {
+        model.movies = movies;
+        console.log(model.movies);
+      });
+    };
+
+    model.upRating = function(movie) {
+      if (movie.rating < 5) {
+        movie.rating += 1;
+      }
+    };
+
+    model.downRating = function(movie) {
+      if (movie.rating > 1) {
+        movie.rating -= 1;
+      }
+    };
+  }
+
   module.component("movieList", {
     templateUrl: "/ps-movies/movie-list.component.html",
     controllerAs: "model",
-    controller: function() {
-      var model = this;
-      model.message = "hello from a component controller!";
-
-      model.changeMessage = function() {
-        model.message = "new message";
-      };
-    }
+    controller: ["$http", controller]
   });
 })();
