4d247bcfa2b107e0eaf6d33f2975d858a87199b4
liming wu <limwuly@gmail.com>
2019-11-24 17:14:20 -0500

    show star/star-empty

--- ps-movies/movie-rating.component.js
+++ ps-movies/movie-rating.component.js
@@ -3,20 +3,31 @@
 
   var module = angular.module("psMovies");
 
+  function buildEntry(value, max) {
+    var entries = [];
+    for (var i = 1; i <= max; i++) {
+      var entry = i <= value ? "glyphicon-star" : "glyphicon-star-empty";
+      entries.push(entry);
+    }
+
+    return entries;
+  }
+
   module.component("movieRating", {
     templateUrl: "/ps-movies/movie-rating.component.html",
     bindings: {
-      value: "<"
+      value: "<",
+      max: "<"
     },
-    transclude:true,
     controllerAs: "model",
     controller: function() {
       var model = this;
+
       model.$onInit = function() {
-        model.entries = new Array(model.value);
+        model.entries = buildEntry(model.value, model.max);
       };
       model.$onChanges = function() {
-        model.entries = new Array(model.value);
+        model.entries = buildEntry(model.value, model.max);
       };
     }
   });
