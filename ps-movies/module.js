37dfb8fda629a5b5144124d0fb2ca6fd19149de2
liming wu <limwuly@gmail.com>
2019-11-23 21:12:48 -0500

    initialize

---
+++ ps-movies/module.js
@@ -0,0 +1,14 @@
+(function() {
+  "use strict";
+
+  var module = angular.module("psMovies", []);
+
+  module.config(function($locationProvider) {
+    $locationProvider.html5Mode(true);
+  });
+
+  module.value("$routerRootComponent", "movieApp");
+  module.component("appAbout", {
+    template: "This is the about page"
+  });
+})();
