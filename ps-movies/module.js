5d7b1d52cd7b4b5af5d585045b8f708bb7908c66
liming wu <limwuly@gmail.com>
2019-11-24 14:41:34 -0500

    setup component router

--- ps-movies/module.js
+++ ps-movies/module.js
@@ -1,25 +1,16 @@
 (function() {
   "use strict";
 
-  var module = angular.module("psMovies", ["ngRoute"]);
+  var module = angular.module("psMovies", ["ngComponentRouter"]);
 
-  module.config(function($routeProvider) {
-    $routeProvider
-      .when("/list", { template: "<movie-list></movie-list>" })
-      .when("/about", { template: "<app-about></app-about>" })
-      .otherwise({ redirectTo: "/list" });
-  });
-
-  module.component("appAbout", {
-    template: "this is about page"
-  });
+ 
 
   // module.config(function($locationProvider) {
   //   $locationProvider.html5Mode(true);
   // });
 
-  // module.value("$routerRootComponent", "movieApp");
-  // module.component("appAbout", {
-  //   template: "This is the about page"
-  // });
+   module.value("$routerRootComponent", "movieApp");
+  module.component("appAbout", {
+    template: "This is the about page"
+  });
 })();
