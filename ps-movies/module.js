97fa4283c99ce964ee6b376f0b8e456f59180af6
liming wu <limwuly@gmail.com>
2019-11-24 12:03:19 -0500

    use angular-router 1.5.3

--- ps-movies/module.js
+++ ps-movies/module.js
@@ -1,14 +1,25 @@
 (function() {
   "use strict";
 
-  var module = angular.module("psMovies", []);
+  var module = angular.module("psMovies", ["ngRoute"]);
 
-  module.config(function($locationProvider) {
-    $locationProvider.html5Mode(true);
+  module.config(function($routeProvider) {
+    $routeProvider
+      .when("/list", { template: "<movie-list></movie-list>" })
+      .when("/about", { template: "<app-about></app-about>" })
+      .otherwise({ redirectTo: "/list" });
   });
 
-  module.value("$routerRootComponent", "movieApp");
   module.component("appAbout", {
-    template: "This is the about page"
+    template: "this is about page"
   });
+
+  // module.config(function($locationProvider) {
+  //   $locationProvider.html5Mode(true);
+  // });
+
+  // module.value("$routerRootComponent", "movieApp");
+  // module.component("appAbout", {
+  //   template: "This is the about page"
+  // });
 })();
